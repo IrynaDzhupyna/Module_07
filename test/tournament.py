65117b42b49c6eddda9c352b5d55ae0507a33c90
Iryna Dzhupyna <irdzhupy@c3a8c1.42berlin.de>
2026-06-17 13:55:39 +0200

    ex2

--- test/tournament.py
+++ test/tournament.py
@@ -4,28 +4,42 @@ from ex2 import NormalStrategy, AggresiveStrategy, DefensiveStrategy
 
 
 def battle(opponents: list[tuple]) -> None:
-    print(f" {opponents}")
+    # print(some info)
     print("*** Tournament ***")
     print(f"{len(opponents)} opponents involved\n")
+    for i in range(len(opponents)):
+        for j in range(i + 1, len(opponents)):
+            factory_1, strategy_1 = opponents[i]
+            factory_2, strategy_2 = opponents[j]
+            creature_1 = factory_1.create_base()
+            creature_2 = factory_2.create_base()
     print("* Battle *")
+    print(f"{creature_1.describe()}\n vs\n{creature_2.describe()}")
+    print(" now fight!")
+    print(creature_1.attack())
+    print(creature_2.attack())
+    
+
 
 
 def main() -> None:
     # players
-    Flameling = FlameFactory().create_base()
+    """lameling = FlameFactory().create_base()
     aqua = AquaFactory()
-    base = aqua.create_base()
-    # Aquabub = AquaFactory.create_base()
+    Aquabub = AquaFactory.create_base()
     Healing = HealingCreatureFactory.create_base()
     shiftling = TransformCreatureFactory().create_base()
 
     # strategies
     Normal = NormalStrategy()
     aggresive = AggresiveStrategy()
-    Defensive = DefensiveStrategy()
+    Defensive = DefensiveStrategy()"""
 
     print("Tournament 0 (basic)")
-    battle([(Flameling, Normal), (Healing, Defensive)])
+    opponents = [
+        (FlameFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy())]
+    battle(opponents)
 
 
 
